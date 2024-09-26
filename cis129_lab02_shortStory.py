6a9d69475a2d08113fb0d759a0c16f0a3b1e75a8
mbarajas828 <mbarajas8@mail.pima.edu>
2024-09-25 23:31:17 -0700

    Update jellyfish story

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,3 +1,9 @@
 #author: Mia Barajas
 # My first Python lab in my CIS129 course 
-#print once there was a lost princess who wandered far from home to the ocean. an octopus swims by and, seeing the princess's locks of hair in the water, mistakes it for another octopus. the octopus falls in love. When he pokes his head above the water and sees the princess, he vows to help her return home. He took the princess across the ocean and eventually returned her to her family's castle. As thanks, the princess cut her hair and gifted the wrapped locks to the octopus for him to carry around as company. over time, the currents begin to untie the hair and one day, as the octopus rushes to grab the hair, he ties his tentacles into knots. In his struggle, he tears himself to shreds, but each piece of the octopus's body comes back to life with strands of hair.
+#print once there was a lost princess who wandered far from home to the ocean. an octopus swims by and,
+seeing the princess's locks of hair in the water, mistakes it for another octopus. the octopus falls in love. When he
+pokes his head above the water and sees the princess, he vows to help her return home. He took the princess across the
+ocean and eventually returned her to her family's castle. As thanks, the princess cut her hair and gifted the wrapped locks
+to the octopus for him to carry around as company. over time, the currents begin to untie the hair and one day, as the octopus
+rushes to grab the hair, he ties his tentacles into knots. In his struggle, he tears himself to shreds, but each piece of the
+octopus's body comes back to life with strands of hair. this is where my mom says jellyfish comes from.
